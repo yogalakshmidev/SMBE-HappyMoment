f6652502006d166de56ef40638344079fca557d0
Yogalakshmi <yogarajakanibe@gmail.com>
2025-05-19 18:21:55 +0530

    cors url added

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ const PORT = process.env.PORT;
 
 const app = express();
 app.use('/images',express.static('public/images'))
-app.use(cors());
+// app.use(cors());
+
+// enable cors
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
